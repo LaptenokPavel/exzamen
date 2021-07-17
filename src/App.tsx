96ac6b4d21fb0fccded696d1de4da910b8eb2e3e
Laptenok Pavel <pavlap82@mail.ru>
2021-07-17 14:01:04 +0300

    "17.07"

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+import './App.module.css';
+import {Counter} from "./Counter";
+
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    let startValue = 0;
+    let finishValue = 5;
 
+    let [data, setData] = useState<number>(startValue);
+
+
+   return (
+       <div>
+<Counter
+    data={data}
+    setData={setData}
+    startValue={startValue}
+    finishValue={finishValue}
+/>
+</div>
+    )
+}
 export default App;
