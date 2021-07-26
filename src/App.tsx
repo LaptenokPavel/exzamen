35eee4d44f403b40262f6dd0d8131adf79893a96
LaptenokPavel <1982pavlap@gmail.com>
2021-07-26 20:11:15 +0300

    26.07-6

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import './App.module.css';
 import {Counter} from "./Counter";
 
 
-
 function App() {
     let startValue = 0;
     let finishValue = 5;
@@ -11,19 +10,16 @@ function App() {
     let [data, setData] = useState<number>(startValue);
 
 
-   return (
-       <div>
-           <h2>Hello</h2>
-           <h3>word!</h3>
-           <h4>YO</h4>
-
-<Counter
-    data={data}
-    setData={setData}
-    startValue={startValue}
-    finishValue={finishValue}
-/>
-</div>
+    return (
+        <div>
+            <Counter
+                data={data}
+                setData={setData}
+                startValue={startValue}
+                finishValue={finishValue}
+            />
+        </div>
     )
 }
+
 export default App;
